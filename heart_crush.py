e28663e43a994d5f1fc5c32e79722c5809d5db98
padovani <henriquepalima@hotmail.com>
2024-04-05 00:06:57 -0300

    master

---
+++ heart_crush.py
@@ -0,0 +1,14 @@
+from turtle import *
+from time import sleep
+
+color("blue")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+sleep(4)
